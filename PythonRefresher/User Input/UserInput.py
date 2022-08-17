6b980c6da1d1e6c7e420ae9888f940361b357714
JOEUN PARK <mrnglory@mail.hongik.ac.kr>
2022-08-17 21:04:15 +0900

    [python refresher] user input

---
+++ PythonRefresher/User Input/UserInput.py
@@ -0,0 +1,7 @@
+"""
+User Input
+"""
+
+first_name = input("Enter your first name: ")
+days = input("How many days before yur birthday?: ")
+print(f"Hi {first_name}, only {days} days before your birthday!")
