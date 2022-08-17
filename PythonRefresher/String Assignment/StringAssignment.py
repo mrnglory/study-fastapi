6fb40f09e0920473fdc5d9f6352f2d78b0aedad3
JOEUN PARK <mrnglory@mail.hongik.ac.kr>
2022-08-17 21:11:33 +0900

    [python refresher] assignment: string

---
+++ PythonRefresher/String Assignment/StringAssignment.py
@@ -0,0 +1,14 @@
+"""
+String Assignment:
+
+Ask the user how many days until their birthday
+and print on approx number of weeks until their birthday
+
+Weeks is = 7 days
+
+decimals within the return is allowed...
+"""
+
+days = int(input("How many days until your birthday?: "))
+
+print(round(days / 7, 2))
