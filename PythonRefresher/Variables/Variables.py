90165ae824fb2e54836454c9c19e208c038c5a4f
JOEUN PARK <mrnglory@mail.hongik.ac.kr>
2022-08-17 20:42:56 +0900

    [python refresher] variables

---
+++ PythonRefresher/Variables/Variables.py
@@ -0,0 +1,29 @@
+"""
+Variables in Python
+"""
+
+cost = 10
+tax_percent = .25
+tax = cost * tax_percent
+price = cost + tax
+
+print(price)
+
+
+username = 'Soju'
+first_name = 'Joeun'
+print(username + ' ' + first_name)
+
+
+first_num = 10
+second_num = 2
+print(first_num)
+print(second_num)
+
+first_num = 1
+print(first_num)
+print(second_num)
+
+
+first_name = 'Melissa'
+print(first_name)
