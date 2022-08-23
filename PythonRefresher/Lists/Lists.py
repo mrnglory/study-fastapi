95540dbe1ced40d4a7ae7efd97d6ab2f4f203002
joeun <standup008@gmail.com>
2022-08-24 08:43:54 +0900

    [python refresher] lists

---
+++ PythonRefresher/Lists/Lists.py
@@ -0,0 +1,19 @@
+"""
+Lists are a collection of data
+"""
+
+
+my_list = [80, 96, 72, 100, 8]
+print(my_list)
+my_list.append(1000)
+print(my_list)
+my_list.insert(2, 1000)
+print(my_list)
+my_list.remove(8)
+print(my_list)
+my_list.pop(0)
+print(my_list)
+
+people_list = ["Eric", "Adil", "Jeff"]
+people_list[0] = "Mel"
+print(people_list)
