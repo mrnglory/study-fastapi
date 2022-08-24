1f89b4a7b117524a3c885b481ced480b7deba573
joeun <standup008@gmail.com>
2022-08-24 11:05:00 +0900

    [python refresher] if else statements

---
+++ PythonRefresher/If Else/IfElse.py
@@ -0,0 +1,23 @@
+"""
+Flow Control: If Else Elif
+"""
+
+
+x = 0
+
+if x > 1:
+    print("x is greater than 1")
+else:
+    print("x is not grater than 1")
+
+print("outside of if statement")
+
+
+hour = 18
+
+if hour < 15:
+    print("Good morning!")
+elif hour < 20:
+    print("Good afternoon!")
+else:
+    print("Good night!")
